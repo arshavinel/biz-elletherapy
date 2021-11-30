285c61ec11fb1028badf27fa5d5342fa8253eca0
Wallentyn Arshavin <arsavinwallentyn@gmail.com>
2021-11-30 13:28:44 +0200

    #9 - banner piece added in 404/site

--- outcomes/404/site/frontend.php
+++ outcomes/404/site/frontend.php
@@ -1,8 +1,5 @@
-<div class="section-banner--with-text" style="background-image: url(<?= Brain\View\Site::image('banner.background', 1400, 450) ?>);">
-    <div class="overlay">
-        <div class="container">
-            <h1 class="sentence"><?= Brain\View\Site::sentence('banner.text') ?></h1>
-            <p class="tag"><?= Brain\View\Site::sentence('banner.pagina') ?></p>
-        </div>
-    </div>
-</div>
+<?= Arsh\Core\Piece::html('site/banner', array(
+    'background'    => Brain\View\Site::image('banner.background', 1400, 450),
+    'sentence'      => Brain\View\Site::sentence('banner.text'),
+    'tag'           => Brain\View\Site::sentence('banner.pagina')
+)) ?>
