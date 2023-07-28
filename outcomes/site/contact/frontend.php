1b5781e7975e6b14ad9d33ab8c2548c034f6a594
Arshavin <arsavinwallentyn@gmail.com>
2023-07-28 19:50:02 +0300

    My IP updated

--- outcomes/site/contact/frontend.php
+++ outcomes/site/contact/frontend.php
@@ -5,46 +5,48 @@
 )) ?>
 
 <div id="section--letter" class="container margin-3rd-3rd">
-    <div class="padding-3rd-0 shadow bg-color-7 px-3">
+    <div class="padding-2nd-0 shadow bg-color-7">
 
-        <div class="row">
-            <div class="col-12 padding-0-5th">
-                <?= Arshavinel\ElleTherapy\View\Site::content('box.text') ?>
-            </div>
+        <div class="px-3">
+            <div class="row">
+                <div class="col-12 padding-0-5th">
+                    <?= Arshavinel\ElleTherapy\View\Site::content('box.text') ?>
+                </div>
 
-            <div class="col-md-6">
-                <div class="border border-color-2 text-color-2 p-3 pb-2 h-100">
-                    <h1><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.titlu') ?></h1>
+                <div class="col-md-6">
+                    <div class="border border-color-2 text-color-2 p-3 pb-2 h-100">
+                        <h1><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.titlu') ?></h1>
 
-                    <div class="padding-1st-1st">
-                        <div>
-                            <b><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.email') ?></b>
-                            <a href="mailto: <?= str_replace(' ', '', Arshavinel\ElleTherapy\Table\Config::title('email')) ?>">
-                                <?= Arshavinel\ElleTherapy\Table\Config::title('email') ?>
-                            </a>
-                        </div>
-                        <div>
-                            <b><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.adresa') ?></b>
-                            <?= Arshavinel\ElleTherapy\View\Site::content('adresă.romania', NULL, true) ?>
+                        <div class="padding-1st-1st">
+                            <div>
+                                <b><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.email') ?></b>
+                                <a href="mailto: <?= str_replace(' ', '', Arshavinel\ElleTherapy\Table\Config::title('email')) ?>">
+                                    <?= Arshavinel\ElleTherapy\Table\Config::title('email') ?>
+                                </a>
+                            </div>
+                            <div>
+                                <b><?= Arshavinel\ElleTherapy\View\Site::sentence('contact.adresa') ?></b>
+                                <?= Arshavinel\ElleTherapy\View\Site::content('adresă.romania', NULL, true) ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 mt-3 mt-md-0">
-                <div class="border border-color-3 px-3 h-100">
-                    <?php
-                    foreach ($social_media as $media) { ?>
-                        <div class="media padding-3rd-3rd padding-md-1st-1st">
-                            <a href="<?= $media->link ?>">
-                                <img src="<?= $media->file('icon')->url('tinny') ?>" class="align-self-center mr-3" alt="<?= $media->translation('text') ?>">
-                            </a>
-                            <div class="media-body align-self-center">
-                                <a href="<?= $media->link ?>" class="text-color-4">
-                                    <?= $media->translation('text') ?>
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <div class="border border-color-3 px-3 h-100">
+                        <?php
+                        foreach ($social_media as $media) { ?>
+                            <div class="media padding-3rd-3rd padding-md-1st-1st">
+                                <a href="<?= $media->link ?>">
+                                    <img src="<?= $media->file('icon')->url('tinny') ?>" class="align-self-center mr-3" alt="<?= $media->translation('text') ?>">
                                 </a>
+                                <div class="media-body align-self-center">
+                                    <a href="<?= $media->link ?>" class="text-color-4">
+                                        <?= $media->translation('text') ?>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    <?php } ?>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
         </div>
